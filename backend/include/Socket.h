ec719aed5ec74e70e7f69755baa4ff646d11931e
RoyalInTheNight <107641578+RoyalInTheNight@users.noreply.github.com>
2023-06-04 01:51:32 +0500

    Update Socket.h

--- backend/include/Socket.h
+++ backend/include/Socket.h
@@ -1,20 +1,35 @@
 #ifndef SOCKET_ARCH
 #define SOCKET_ARCH
 
-#include <thread_arch.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include "core.h"
+
+typedef sockaddr_in   ssock;
+typedef sockaddr       sock;
+typedef sockaddr *   __sock;
+typedef sockaddr_in __ssock;
+typedef socklen_t     sslen;
+typedef socklen_t * __sslen;
+typedef int          SOCKET;
+
+using namespace core::types;
 
 class Socket {
 private:
-    typedef int __It;
+    static inline u32 strlen(const _s8);
 
-    __sock        sock_main;
-    __sock     sock_reserve;
-    __sslen       sock_size;
-
-    __It              _part;
+    static u32 ferr_buffer;
 
 public:
-
+    static inline void socket(SOCKET *, u32);
+    static inline void bind(SOCKET, const __sock, sslen);
+    static inline void send(SOCKET, _s8);
+    static inline void recv(SOCKET, _s8, u64);
+    static inline void sendto(SOCKET, _s8, const __sock, sslen);
+    static inline void recvfrom(SOCKET, const _s8, const __sock, const __sslen);
 };
 
 #endif // SOCKET_ARCH
